83abc12889bc4ff25ec6c25fc2b4d8b913d0758c
kumaran <k31311558@gmail.com>
2026-05-27 19:11:03 +0530

    Update main.asm

--- firmware/main.asm
+++ firmware/main.asm
@@ -1,5 +1,4 @@
 INCLUDE REG51.INC
-INCLUDE macros.inc
 
 ORG 0000H
 
@@ -9,9 +8,39 @@ MAIN:
 
 MAIN_LOOP:
 
-    ACALL READ_BUTTONS
+; ======================================
+; SEND BUTTON BYTE 1
+; ======================================
 
+    ACALL READ_BUTTONS_1
     ACALL UART_SEND
+
+; ======================================
+; SEND BUTTON BYTE 2
+; ======================================
+
+    ACALL READ_BUTTONS_2
+    ACALL UART_SEND
+
+; ======================================
+; SEND JOYSTICK VALUES
+; ======================================
+
+    ACALL READ_LX
+    ACALL UART_SEND
+
+    ACALL READ_LY
+    ACALL UART_SEND
+
+    ACALL READ_RX
+    ACALL UART_SEND
+
+    ACALL READ_RY
+    ACALL UART_SEND
+
+; ======================================
+; SMALL DELAY
+; ======================================
 
     ACALL DELAY
 
