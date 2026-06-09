42d2239c09ca5a731f55b015dd6f26b8f364a745
Kumaran Chandrashekar <k31311558@gmail.com>
2026-06-09 11:20:23 +0530

    Create dssx_runtime.c

---
+++ sdk/c/src/dssx_runtime.c
@@ -0,0 +1,12 @@
+#include "../include/dssx_runtime.h"
+#include <stdio.h>
+
+void dssx_runtime_init()
+{
+    printf("DSSX Runtime Init\n");
+}
+
+void dssx_runtime_shutdown()
+{
+    printf("DSSX Runtime Shutdown\n");
+}
