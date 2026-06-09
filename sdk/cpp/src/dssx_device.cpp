7cf1c2443e40074f850eeaefdbe20a2bcdba95b0
Kumaran Chandrashekar <k31311558@gmail.com>
2026-06-09 11:52:49 +0530

    Create dssx_device.cpp

---
+++ sdk/cpp/src/dssx_device.cpp
@@ -0,0 +1,12 @@
+#include "../include/dssx_device.hpp"
+#include <iostream>
+
+void DSSXDevice::connect()
+{
+    std::cout << "DSSX Device Connected\n";
+}
+
+void DSSXDevice::disconnect()
+{
+    std::cout << "DSSX Device Disconnected\n";
+}
